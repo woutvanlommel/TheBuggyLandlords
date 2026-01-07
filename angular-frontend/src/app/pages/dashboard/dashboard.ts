5aaa9a6129b82acae465dd6899d15a11303288b5
20YN04 <yentl.nerinckx@icloud.com>
2026-01-07 17:07:52 +0100

    Refactor dashboard component to use inline template

--- angular-frontend/src/app/pages/dashboard/dashboard.ts
+++ angular-frontend/src/app/pages/dashboard/dashboard.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   imports: [],
-  templateUrl: './dashboard.html',
+  template: `
+  <div id="dashboard">
+    <div id="dashboardHeader" class="">
+      <span>DASHBOARD</span>
+    </div>
+    <div>
+
+    </div>
+  </div>
+  `
+  ,
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
