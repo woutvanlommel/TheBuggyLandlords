ac3e618a7b7f98bbad22070c49f3719762277f78
woutvanlommel <woutvanlommel@icloud.com>
2026-01-08 12:24:39 +0100

    Navigation in de kotcompass

--- angular-frontend/src/app/pages/kotcompass/kotcompass.ts
+++ angular-frontend/src/app/pages/kotcompass/kotcompass.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterModule } from "@angular/router";
-
-
+import { RouterModule } from '@angular/router';
+import { Navigation } from '../../components/navigation/navigation';
 
 @Component({
   selector: 'app-kotcompass',
-  imports: [RouterModule],
+  imports: [RouterModule, Navigation],
   styleUrl: './kotcompass.css',
   template: `
+    <app-navigation></app-navigation>
     <h1>Kotcompass Page</h1>
     <router-outlet></router-outlet>
   `,
