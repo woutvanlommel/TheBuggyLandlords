efeebb57e9418ca25d728feb300beb8992a8229b
20YN04 <yentl.nerinckx@icloud.com>
2026-01-08 14:18:18 +0100

    feat: add KOTCOMPASS link to footer navigation with enhanced styling

--- angular-frontend/src/app/components/footer/footer.ts
+++ angular-frontend/src/app/components/footer/footer.ts
@@ -25,7 +25,18 @@ import { RouterLink } from '@angular/router';
       </div>
 
       <!-- Middle Section -->
-      <div id="navigationMidden" class="flex flex-col gap-4 w-full lg:w-1/3 items-center justify-center">
+      <div id="navigationMiddle" class="flex flex-col  w-full lg:w-1/3 items-center justify-center">
+        <!-- KOTCOMPASS  -->
+        <a routerLink="/kotcompass" class="focus:outline-none cursor-pointer relative flex items-center justify-center gap-2 mx-auto text-xl font-bold text-gray-900 transition-all duration-300 group hover:-translate-y-1 mb-2">
+          <span class="relative">
+            KOTCOMPASS
+            <span class="absolute bottom-1 left-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
+          </span>
+          <svg class="w-7 h-7 transition-transform duration-300 ease-linear rotate-45 group-hover:rotate-90" viewBox="0 0 16 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z" fill="currentColor"></path>
+          </svg>
+        </a>
+
         <!-- Dashboard -->
         <a routerLink="/dashboard" class="focus:outline-none cursor-pointer relative flex items-center justify-center gap-2 mx-auto text-lg text-gray-800 transition-all duration-300 group hover:-translate-y-1">
           <span class="relative">
