b2cde22532c1b146d4233d4ea422f70d777d8746
AMicciche <alessiomicciche@outlook.com>
2026-01-09 11:37:43 +0100

    fix: implement smooth scrolling behavior for main content and custom scrollbar

--- angular-frontend/src/app/app.ts
+++ angular-frontend/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, OnInit } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd, Event } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,24 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('angular-frontend');
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe(() => {
+
+      setTimeout(() => {
+
+      const mainContent = document.querySelector('.custom-scrollbar');
+      if (mainContent) {
+        mainContent.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+      window.scrollTo({ top: 0, behavior: 'smooth'});
+    }, 10)
+    });
+  }
 }
