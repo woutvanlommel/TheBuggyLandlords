ea3f803958bce8eb68bb8f6c6ae517902f3c27de
AMicciche <alessiomicciche@outlook.com>
2026-01-09 12:51:43 +0100

    feat: add HasFactory trait and define fillable properties in Favorite model

--- laravel-backend/app/Models/Favorite.php
+++ laravel-backend/app/Models/Favorite.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'room_id',
+    ];
 }
