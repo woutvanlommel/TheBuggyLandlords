22dd035272af71a36ad626d48cd636c0bcd7cb11
20YN04 <yentl.nerinckx@icloud.com>
2026-01-13 17:35:20 +0100

    feat: implement footer visibility logic and manage router subscription in Kotcompass component

--- angular-frontend/src/app/pages/kotcompass/kotcompass.ts
+++ angular-frontend/src/app/pages/kotcompass/kotcompass.ts
@@ -1,16 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { Navigation } from '../../components/navigation/navigation';
 import { Footer } from '../../components/footer/footer';
+import { CommonModule } from '@angular/common';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-kotcompass',
-  imports: [Footer, Navigation, RouterModule],
+  imports: [Footer, Navigation, RouterModule, CommonModule],
   styleUrl: './kotcompass.css',
   template: `
     <app-navigation></app-navigation>
     <router-outlet></router-outlet>
+    @if (showFooter) {
     <app-footer></app-footer>
+    }
   `,
 })
-export class Kotcompass {}
+export class Kotcompass implements OnInit, OnDestroy {
+  showFooter = true;
+  private routerSubscription!: Subscription;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.checkFooterVisibility();
+    this.routerSubscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.checkFooterVisibility();
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
+
+  private checkFooterVisibility() {
+    const url = this.router.url.split('?')[0];
+    this.showFooter = url !== '/kotcompass/rooms';
+  }
+}
